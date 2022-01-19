e7ca7146e162c0d8d3011b3e02b1005e9bc94c73
Ronin <96494366+Ronin885@users.noreply.github.com>
2022-01-19 20:25:09 +0300

    Add files via upload

---
+++ Project14.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+
+    int count;
+    int cha;
+    int numZ;
+    int totalcost = 0;
+    cout << "klient : "; 
+    cin >> count; 
+    cout << endl;
+    
+    for (int i = 1; i <= count; i++) 
+    {
+        cout << "zakazbl : ";
+        cin >> cha; 
+        cout << endl;
+        for (int v = 1; v <= cha; v++)
+        {
+            cin >> numZ;
+            switch (numZ) {
+            case 1:
+            
+                totalcost += 15;
+                cout << 228;
+                break;
+            
+            case 2:
+                totalcost += 10;
+                break;
+            case 3:
+                totalcost += 5;
+           
+                break;
+           
+            }
+
+
+        }
+        
+    }
+    cout << totalcost;
+}
